47c557ccd901192d661bd60fdb3756a931624f51
mondragon-ai <angel.mondragon@yahoo.com>
2025-01-02 11:02:56 -0300

    middleware & api

--- main.go
+++ main.go
@@ -3,16 +3,27 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main () {
 	fmt.Println("starting go server....")
 
 	fileServer := http.FileServer(http.Dir("."))
 
-	mux := http.NewServeMux()
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
 
-	mux.Handle("/", fileServer)
+	mux := http.NewServeMux()
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", fileServer)))
+	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", apiCfg.handleMetrics)
+	mux.HandleFunc("/resets", apiCfg.handleReset)
 	
 	server := &http.Server{
 		Addr: ":8080",
@@ -22,4 +33,18 @@ func main () {
 	if err := server.ListenAndServe(); err != nil {
 		panic(err)
 	}
+}
+
+func (cfg *apiConfig) handleMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+}
+
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
 }
